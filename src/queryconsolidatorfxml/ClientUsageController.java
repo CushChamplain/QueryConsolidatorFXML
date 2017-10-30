a423dd373172d235a953afc77d7068659be7168c
cushhelfrich <cushhelfrich@hotmail.com>
2017-10-30 01:22:55 -0400

    remove unused imports

--- src/queryconsolidatorfxml/ClientUsageController.java
+++ src/queryconsolidatorfxml/ClientUsageController.java
@@ -12,22 +12,15 @@ package queryconsolidatorfxml;
  */
 //import java.sql.Connection;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import static queryconsolidatorfxml.QueryConsolidatorFXML.getMainFXML;
 
 /**
  * FXML Controller class
